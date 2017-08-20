2f30375b385e91ceaf5de7e2d0f028bc21cb7e2f
legokichi <legokichi@gmail.com>
2017-08-20 01:33:35 +0000

    h

--- build.sbt
+++ build.sbt
@@ -2,5 +2,9 @@ name := "scala-sandbox"
 
 version := "0.1"
 
-scalaVersion := "2.12.3"
-        
+scalaVersion := "2.11.11"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "2.2.0",
+  "org.scalaz" %% "scalaz-core" % "7.2.14"
+)
--- src/main/scala/main/HelloWorld.scala
+++ src/main/scala/main/HelloWorld.scala
@@ -1,7 +1,15 @@
 package main
 
+import org.apache.spark._
+
+
 object HelloWorld{
   def main (args: Array[String] ): Unit = {
     println("hello world")
+    val conf = new SparkConf().setAppName("HelloSpark").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val count = sc.parallelize(Array(1,2,3,4,5,6,7,8,9)).count()
+    println(count)
+    System.exit(0)
   }
 }
